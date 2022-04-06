d5f6a4252bbaab5d4a52152fb96171a83af95b00
Ajeet <ajeets.shah@hotmail.com>
2022-04-06 23:07:37 +0530

    Add lombok

--- src/main/java/com/example/demo/CustomerResponse.java
+++ src/main/java/com/example/demo/CustomerResponse.java
@@ -1,28 +1,17 @@
 package com.example.demo;
 
+import lombok.*;
+
+@Setter
+@Getter
 public class CustomerResponse {
 
-    CustomerResponse(String firstName, String lastName) {
+    public CustomerResponse(String firstName, String lastName) {
         this.firstName = firstName;
         this.lastName = lastName;
     }
 
-    private String firstName;
-    private String lastName;
+    public String firstName;
+    public String lastName;
 
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
 }

859454ff8273bfe0f9520bc2e34f9f1484c57901
Ajeet <ajeets.shah@hotmail.com>
2022-04-12 22:38:35 +0530

    Add task verify the documents

--- src/main/java/com/example/demo/process/ProcessController.java
+++ src/main/java/com/example/demo/process/ProcessController.java
@@ -1,6 +1,6 @@
 package com.example.demo.process;
 
-import com.example.demo.customer.CustomerRepository;
+import org.camunda.bpm.engine.ProcessEngine;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,17 +10,26 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(value = "/process")
 public class ProcessController {
-    @Autowired
-    CustomerRepository repository;
 
     @Autowired
     CamundaService camundaService;
 
-    @GetMapping("/start/{businessKey}")
+    @Autowired
+    ProcessEngine processEngine;
+
+    @GetMapping("/start/business/{businessKey}")
     public String startProcess(@PathVariable("businessKey") Integer businessKey){
-        String message = "Starting the process";
-        System.out.println(message);
         camundaService.startProcess(businessKey);
+        String message = "Starting a process with businessKey: " + businessKey;
+        System.out.println(message);
+        return message;
+    }
+
+    @GetMapping("/complete/tasks/{taskId}")
+    public String completeTask(@PathVariable("taskId") String taskId) {
+        processEngine.getTaskService().complete(taskId);
+        String message = "Completed the task: " + taskId;
+        System.out.println(message);
         return message;
     }
 

c7da0822f18c994360e22f3852a89f7324c76a66
Ajeet <ajeets.shah@hotmail.com>
2022-04-12 18:12:34 +0530

    Add loan approval bpmn file

--- src/main/java/com/example/demo/SpringBootCamunda.java
+++ src/main/java/com/example/demo/SpringBootCamunda.java
@@ -1,13 +1,25 @@
 package com.example.demo;
 
+import org.camunda.bpm.engine.RuntimeService;
+import org.camunda.bpm.spring.boot.starter.annotation.EnableProcessApplication;
+import org.hibernate.event.spi.PostDeleteEvent;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableProcessApplication
 public class SpringBootCamunda {
+
+	@Autowired
+	private RuntimeService runtimeService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootCamunda.class, args);
+	}
+
+	private void processPostDeploy(PostDeleteEvent postDeleteEvent) {
+		runtimeService.startProcessInstanceByKey("loanApproval");
 	}
 
 }
